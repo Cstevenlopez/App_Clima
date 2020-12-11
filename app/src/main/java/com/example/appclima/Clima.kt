a64fb11b38ff53eb233388580c23c19ba9ddc0f1
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-12-10 20:27:27 -0600

    Termine la app

--- app/src/main/java/com/example/appclima/Clima.kt
+++ app/src/main/java/com/example/appclima/Clima.kt
@@ -3,11 +3,13 @@ package com.example.appclima
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import java.lang.Exception
 import kotlin.math.log
 
@@ -17,7 +19,8 @@ class Clima : AppCompatActivity() {
         setContentView(R.layout.activity_clima)
         if(Network.hayRed(this))
         {
-
+            Toast.makeText(this, "Hay red", Toast.LENGTH_SHORT).show()
+            solicitudHTTPVolley("https://api.openweathermap.org/data/2.5/weather?id=3616253&appid=ed006f0d32b20db6da1aaf5008f20bcc&lang=es&units=metric")
         }else{
             Toast.makeText(this, "No hay red",Toast.LENGTH_SHORT).show()
         }
@@ -28,13 +31,25 @@ class Clima : AppCompatActivity() {
         val solicitud = StringRequest(Request.Method.GET,url,Response.Listener<String>{
            response ->
             try{
-                Log.d("ResultadoVolley",responde)
+                Log.d("ResultadoVolley",response)
+                val gson = Gson()
+                val ciudad = gson.fromJson(response,Ciudad::class.java)
+                val tvGrados: TextView = findViewById(R.id.tvGrados)
+                val tvCiudad: TextView = findViewById(R.id.tvCiudad)
+                val tvDescripcion: TextView = findViewById(R.id.tvDescripcion)
+                tvGrados.text = ciudad.main!!.temp.toString()
+                tvCiudad.text = ciudad.name
+                tvDescripcion.text = ciudad.weather!!.get(0).description
+
+
+
             } catch (e:Exception){
 
             }
 
         },Response.ErrorListener {
-
+        Log.e("ErrorVolley","Error Extra;o")
         })
+        queve.add(solicitud)
     }
 }
