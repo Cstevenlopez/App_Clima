1f528d7479f1bce0fbc00ea697248962ab511c94
Cstevenlopez <cstevenlopez2001@gmail.com>
2020-12-10 19:32:04 -0600

    Agrege codigo

--- app/src/main/java/com/example/appclima/Clima.kt
+++ app/src/main/java/com/example/appclima/Clima.kt
@@ -2,10 +2,39 @@ package com.example.appclima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.lang.Exception
+import kotlin.math.log
 
 class Clima : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_clima2)
+        setContentView(R.layout.activity_clima)
+        if(Network.hayRed(this))
+        {
+
+        }else{
+            Toast.makeText(this, "No hay red",Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun solicitudHTTPVolley(url:String)
+    {
+        val queve =Volley.newRequestQueue(this)
+        val solicitud = StringRequest(Request.Method.GET,url,Response.Listener<String>{
+           response ->
+            try{
+                Log.d("ResultadoVolley",responde)
+            } catch (e:Exception){
+
+            }
+
+        },Response.ErrorListener {
+
+        })
     }
 }
